61571ed410aba0211e971567565e6ad4303ac0b1
Evgenestan <61114102+Evgenestan@users.noreply.github.com>
2020-02-16 21:28:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libtest;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            data.vsquare(4,6,6);
+            Console.ReadLine();
+
+        }
+    }
+}
